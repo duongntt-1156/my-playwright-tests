b17b1eb91f66f298bd400e0fafe816bbecea2107
nguyen.t.t.duongb <nguyen.t.t.duongb@sun-asterisk.com>
2026-03-31 15:15:48 +0700

    Update BTTH TCSearch test

--- BTTH-Test Case/TCSearch.spec.ts
+++ BTTH-Test Case/TCSearch.spec.ts
@@ -1,9 +1,10 @@
 import { test, expect } from '@playwright/test';
 
-test('Search on W3Schools', async ({ page }) => {
+test('TCSearch', async ({ page }) => {
   await page.goto('https://www.w3schools.com/');
 
   const searchInput = page.locator('#tnb-google-search-input');
+  await expect(searchInput).toBeVisible();
   await searchInput.fill('HTML');
   await searchInput.press('Enter');
 });
